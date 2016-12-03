3fd3d8332b20e26614f0f1e4da3c8f014e3b02a7
Shougat <shougat@rokomari.com>
2016-12-03 16:32:14 +0600

    Implement routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero'
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit { 
+export class AppComponent { 
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
-  constructor(private heroService: HeroService) { }
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'my-hero-detail',
     templateUrl: './hero-detail.component.html' 
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
     @Input()
     hero: Hero;
 }
